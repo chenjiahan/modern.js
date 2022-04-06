73bb1753e4ab8d3d46b9afd0e47ff323c1d31558
chenjiahan <chenjiahan.jait@bytedance.com>
2022-04-06 11:21:34 +0800

    chore: group integration tests

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,3 +1,39 @@
+const fs = require('fs');
+const { join } = require('path');
+
+const GROUP_MAP = {
+  css: ['css', 'css-modules'],
+  server: ['server-prod', 'bff-express', 'api-service-koa'],
+  runtime: ['runtime'],
+};
+
+const mapFolderToMatch = dirs =>
+  dirs.map(dir => `<rootDir>/integration/${dir}/**/*.(spec|test).[tj]s?(x)`);
+
+const getTestMatch = groupName => {
+  if (groupName) {
+    if (GROUP_MAP[groupName]) {
+      return mapFolderToMatch(GROUP_MAP[groupName]);
+    }
+    throw new Error(`unknown group name: ${groupName}!`);
+  }
+
+  const integrationDir = join(__dirname, 'integration');
+
+  const allDirs = fs
+    .readdirSync(integrationDir)
+    .filter(dir => fs.lstatSync(join(integrationDir, dir)).isDirectory());
+
+  const groupDirs = Object.keys(GROUP_MAP).reduce(
+    (ret, key) => [...ret, ...GROUP_MAP[key]],
+    [],
+  );
+
+  const restDirs = allDirs.filter(dir => !groupDirs.includes(dir));
+
+  return mapFolderToMatch(restDirs);
+};
+
 module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
@@ -5,7 +41,7 @@ module.exports = {
   preset: 'jest-puppeteer',
   rootDir: __dirname,
   setupFilesAfterEnv: ['./utils/jest.setup.js'],
-  testMatch: ['<rootDir>/integration/**/*.(spec|test).[tj]s?(x)'],
+  testMatch: getTestMatch(process.env.TEST_GROUP),
   testPathIgnorePatterns: ['/node_modules/', '/api-service-koa/api/'],
   transform: {
     '^.+.tsx?$': 'ts-jest',
